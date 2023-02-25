cbbc4ac83de01f7adb9888a89dc25aebf9195d87
Fabio Santos <fabioov@hotmail.com>
2023-02-25 10:13:58 -0300

    V1

---
+++ zfsantosvehicles/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["vehicles/zfsantosvehicles/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ zfsantosvehicles/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"vehicles/zfsantosvehicles/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
