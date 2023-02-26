f7791f5e14665be3cf438d3c4c49609d01cd5368
Fabio Santos <fabioov@hotmail.com>
2023-02-26 19:57:12 -0300

    Update de tabela feitos.

--- zfsvehicles/webapp/controller/View1.controller.js
+++ zfsvehicles/webapp/controller/View1.controller.js
@@ -10,6 +10,39 @@ sap.ui.define([
         return Controller.extend("vehicles.zfsvehicles.controller.View1", {
             onInit: function () {
 
+            },
+            onFilterSelect: function (oEvent) {
+                var oBinding = this.byId("vehiclesTable").getBinding("items"),
+                    sKey = oEvent.getParameter("key"),
+                    // Array to combine filters
+                    aFilters = [],
+                    fMedium = 400000.0,
+                    fExpensive = 800000.0;
+    
+                if (sKey === "Barato") {
+
+                    aFilters.push(new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.LT, fMedium));
+ 
+                } else if (sKey === "Medio") {
+                    aFilters.push(
+                      new sap.ui.model.Filter({
+                        filters: [ 
+                          new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.GE, fMedium),
+                          new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.LT, fExpensive)
+                        ],
+                        and: true
+                      })
+                    )
+                  } else if (sKey === "Caro") {
+
+                    aFilters.push(new sap.ui.model.Filter("Price", sap.ui.model.FilterOperator.GE, fExpensive));
+
+                }
+    
+                oBinding.filter(aFilters);
+
+                console.log("Filtered items:", oBinding.getCurrentContexts().map(function(c) { return c.getObject() }));
             }
+    
         });
     });
